6013cbef83df0bd51f9891376cafa6198572dd4c
Gustavo <sasaeucomvoce@hotmail.com>
2024-03-20 09:26:21 -0300

    feat: show panels for new contact and edit profile

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -2,6 +2,7 @@ class WhatsAppController {
     constructor() {
         this.elementsPrototype();
         this.loadElements();
+        this.initEvents();
     }
 
     loadElements() {
@@ -63,6 +64,50 @@ class WhatsAppController {
             return this.classList.contains(name);
         }
 
+
+    }
+
+    initEvents() {
+
+        this.el.myPhoto.on('click', e=>{
+
+            this.closeAllLeftPanel();
+            this.el.panelEditProfile.show();
+            setTimeout(() => {
+                this.el.panelEditProfile.addClass('open');
+            }, 300);//delay necessary so the css transition can have time to kick in
+
+        });
+
+        this.el.btnClosePanelEditProfile.on('click', e=>{
+
+            this.el.panelEditProfile.removeClass('open');
+
+        });
+
+        this.el.btnNewContact.on('click', e=>{
+
+            this.closeAllLeftPanel();
+            this.el.panelAddContact.show();
+            setTimeout(() => {
+                this.el.panelAddContact.addClass('open');
+            }, 300);//delay necessary so the css transition can have time to kick in
+
+        });
+
+        this.el.btnClosePanelAddContact.on('click', e=>{
+
+            this.el.panelAddContact.removeClass('open');
+
+        });
+
+
+    }
+
+    closeAllLeftPanel() {
+
+        this.el.panelAddContact.hide();
+        this.el.panelEditProfile.hide();
 
     }
 
